aceffba88295caa72c59c9a6f0ef1d2294ba73f8
Shaurya Sinha <shaurya.sinha2728@gmail.com>
2025-11-24 02:42:06 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_10E8hmIBY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '455aaqs3f0ja2u8nkp052084lh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5kh5th7hpdmttsnd8qmmnsn0d3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
